fb9cfc2f080861cf083e95ecdd162b23dc838d36
nagarjuna-de <nagarjuna.de@gmail.com>
2022-04-19 14:23:35 +0200

    QR code

---
+++ QR_code_detector/README.md
@@ -0,0 +1,28 @@
+# OpenCV QRCodeDetector()  
+#### Introduction  
+The idea behind this project is to `Detect` and `extract` the information of a QR code in the image.  
+For this project, I used Opencv library to detect and extract information from QR codes.  
+  
+#### What to Install
+pip install Opencv-python == 4.5.5  
+pip install matplotlib == 3.2.2  
+pip install numpy == 1.14.6  
+
+#### How to use it  
+1. First Install the required Packages.
+2. There was a folder with name `images` under the parent folder(QR_code_detector).
+3. Place your QR code images in that folder.
+4. Run `main.py` file
+5. It gonna ask you `Enter the image name with extension:`.  
+6. Enter the image name placed in the `images folder`
+7. A popup opens and displays your uploaded image
+8. Press any button on your keyboard to go further.
+9. If QR code is detected in the image, it prints the `information behind the QR code` in terminal.
+10. Another pop up opens and displays/highlight the detected QR code.
+11. If no QR code detected then it prints `No QR code Detected`
+
+#### Technologies/libraries used
+Python == 3.8.13
+Opencv-python == 4.5.5  
+numpy == 1.14.6  
+matplotlib == 3.2.2
---
+++ QR_code_detector/main.py
@@ -0,0 +1,58 @@
+## Import required libraries
+import cv2
+import os
+import matplotlib.pyplot as plt
+
+# Detect the QR code in the image using     cv2.imshow(f'{description}', image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+## Load the image
+img_upload = input('Enter the image name with extension:')
+uploaded_img = os.path.join(os.path.dirname(__file__), 'images', f'{img_upload}')
+
+def show_img(description,image_name):
+    image = cv2.imread(image_name)
+    cv2.imshow(f'{description}', image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+show_img('your uploaded image', uploaded_img)
+
+def qr_code_detector():
+    img = cv2.imread(uploaded_img)
+    qr_data, point, straight_qr = cv2.QRCodeDetector().detectAndDecode(img)
+    return img, qr_data, point, straight_qr
+
+img, qr_data, points, straight_qr = qr_code_detector()
+
+def image_qr_code():
+    global points
+    if len(points)>0:
+        print('QR Code Detected')
+        print('Decoded QR :', qr_data)
+        points = points[0]
+        for i in range(len(points)):
+            pt1 = [int(val) for val in points[i]]
+            pt2 = [int(val) for val in points[(i + 1) % 4]]
+            cv2.line(img, pt1, pt2, color=(255, 0, 0), thickness=3)
+
+        cv2.imshow('Highlighted QR code Image', img)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+        
+    else:
+        print('No QR Code found')
+
+image_qr_code()
+
+#### Code for only qr code image from the uploaded image using QRCodeDetector()
+
+# only_qr = cv2.resize(straight_qr, (400,400))
+# cv2.imshow('only QR code',only_qr)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
+
+
+
